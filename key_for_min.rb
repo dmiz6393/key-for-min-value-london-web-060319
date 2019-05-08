e3be35a5d64266e35327a79248c3b4337c308f66
Danielle Mizrachi <dmiz@brandeis.edu>
2019-05-08 13:59:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,9 +5,10 @@ ikea = {
 }
 
 def key_for_min_value(ikea)
-  return nil if ikea=={}
+smallest_value= nil 
+smallest_key= nil 
 ikea.collect do |furniture, amount|
-if amount<smallest_value
+if smallest_value==nil || amount<smallest_value
   smallest_value=amount
   smallest_key=furniture
 end
